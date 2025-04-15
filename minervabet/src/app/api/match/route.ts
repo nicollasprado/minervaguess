46d7ab2b5bc818cf581f407ea1c7f7cafc0940e9
nicollasprado <nicollasprado.profissional@gmail.com>
2025-04-15 20:48:06 -0300

    feat: added game info get api endpoint

---
+++ minervabet/src/app/api/match/route.ts
@@ -0,0 +1,18 @@
+import axios from "axios";
+import { NextResponse } from "next/server";
+
+export async function GET(req: Request) {
+  const apiKey = process.env.RIOT_API_KEY;
+  const minervaPuuid = process.env.MINERVA_PUUID;
+  const apiUrl = `https://br1.api.riotgames.com/lol/spectator/v5/active-games/by-summoner/${minervaPuuid}`;
+
+  try {
+    const response = await axios.get(apiUrl, { params: { api_key: apiKey } });
+    const game = response.data;
+
+    return new NextResponse(JSON.stringify(game), { status: 200 });
+  } catch {
+    console.error("Error when fetching game data");
+    return new NextResponse("Partida não encontrada", { status: 404 });
+  }
+}
